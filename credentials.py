808098e17636c2476a9ef2fd0db82122a3747c3c
Vadim Maksimushkin <vadimmaksimushkin@gmail.com>
2026-06-02 11:24:56 -0600

    small changes to prevent import errors

--- credentials.py
+++ credentials.py
@@ -6,13 +6,14 @@ try:
 except ImportError:
     pass
 
-GOOGLE_MAPS_API_KEY:      str = os.environ["GOOGLE_MAPS_API_KEY"]
-PLACES_DB_URL:            str = os.environ["PLACES_DB_URL"]
-QUEUE_DB_URL:             str = os.environ["QUEUE_DB_URL"]
-IMAGE_QUEUE_DB_URL:       str = os.environ["IMAGE_QUEUE_DB_URL"]
-R2_USER_TOKEN:            str = os.environ["R2_USER_TOKEN"]
-R2_ACCOUNT_ID:            str = os.environ["R2_ACCOUNT_ID"]
-R2_ACCESS_KEY:            str = os.environ["R2_ACCESS_KEY"]
-R2_SECRET_ACCESS_KEY:     str = os.environ["R2_SECRET_ACCESS_KEY"]
-R2_BUCKET:                str = os.environ["R2_BUCKET"]
-R2_PUBLIC_URL:            str = os.environ["R2_PUBLIC_URL"]
+GOOGLE_MAPS_API_KEY:      str = os.environ.get("GOOGLE_MAPS_API_KEY", "")
+PLACES_DB_URL:            str = os.environ.get("PLACES_DB_URL", "")
+QUEUE_DB_URL:             str = os.environ.get("QUEUE_DB_URL", "")
+IMAGE_QUEUE_DB_URL:       str = os.environ.get("IMAGE_QUEUE_DB_URL", "")
+R2_USER_TOKEN:            str = os.environ.get("R2_USER_TOKEN", "")
+R2_ACCOUNT_ID:            str = os.environ.get("R2_ACCOUNT_ID", "")
+R2_ACCESS_KEY:            str = os.environ.get("R2_ACCESS_KEY", "")
+R2_SECRET_ACCESS_KEY:     str = os.environ.get("R2_SECRET_ACCESS_KEY", "")
+R2_BUCKET:                str = os.environ.get("R2_BUCKET", "")
+R2_PAGES_BUCKET:          str = os.environ.get("R2_PAGES_BUCKET", "")
+R2_PUBLIC_URL:            str = os.environ.get("R2_PUBLIC_URL", "")
